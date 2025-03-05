a69740b78272a35d3cf633c889a50e6654d95d64
toanomercy <htoan2k1@gmail.com>
2025-03-05 08:39:36 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+const homeRoutes = require('./apps/controllers/homecontroller');
+const adminRoutes = require('./apps/controllers/admincontroller');
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/', homeRoutes);
+app.use('/admin', adminRoutes);
+
+app.use("/static", express.static(__dirname + "/public"));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'apps/views'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/admin', (req, res) => {
+    res.render('admin/adminPage')
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
